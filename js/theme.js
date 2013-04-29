838c23a1a4e29e4a5429d3ce6cdf8988bdf29753
Joni Giuro <joni.giuro@gmail.com>
2013-04-29 10:20:27 +0200

    styles and small fixes

--- js/theme.js
+++ js/theme.js
@@ -266,16 +266,6 @@ function createChoco() {
     })
 }
 
-//RANDOM COLOR -- REMOVE FOR FINAL SCRIPT - ONLY TESTING PURPOSES
-function get_random_color() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.round(Math.random() * 15)];
-    }
-    return color;
-}
-
 function setUrlVars(element){
     cutId = element.attr('id').substr(1)
     window.location.hash = cutId;
